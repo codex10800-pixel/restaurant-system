96588a63c7d6a8b07af8b3938ac3b7e9d5764056
codex-pixel <codex10800@gmail.com>
2026-04-28 11:33:08 +0200

    ready for deployment

--- restaurant/settings.py
+++ restaurant/settings.py
@@ -125,6 +125,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
+STATICFILES_DIRS = [
+    BASE_DIR / 'menu' / 'static',
+]
+
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
